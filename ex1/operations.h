2d2554f496748042b32bb4930563aacf0586398d
candi2002 <lethong2806@gmail.com>
2025-03-08 23:47:56 +0700

    Update and rename operation.c to operations.h

---
+++ ex1/operations.h
@@ -0,0 +1,11 @@
+// operations.h
+#ifndef OPERATIONS_H
+#define OPERATIONS_H
+
+double add(double a, double b);
+double subtract(double a, double b);
+double multiply(double a, double b);
+double divide(double a, double b);
+int modulo(int a, int b);
+
+#endif
