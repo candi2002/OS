d3b79713b2a95dfc370d8ebd456e0c8b2f54adf8
candi2002 <lethong2806@gmail.com>
2025-03-08 23:47:36 +0700

    Create operations.c

---
+++ ex1/operations.c
@@ -0,0 +1,22 @@
+// operations.c
+#include "operations.h"
+
+double add(double a, double b) {
+    return a + b;
+}
+
+double subtract(double a, double b) {
+    return a - b;
+}
+
+double multiply(double a, double b) {
+    return a * b;
+}
+
+double divide(double a, double b) {
+    return a / b;
+}
+
+int modulo(int a, int b) {
+    return a % b;
+}
