22525182b06faaa8eb46d44132d779331fbabbc2
candi2002 <lethong2806@gmail.com>
2025-03-25 08:22:23 +0700

    Update bai1.c

--- ex2/exercise/bai1/bai1.c
+++ ex2/exercise/bai1/bai1.c
@@ -5,29 +5,31 @@
 #include <sys/ipc.h>
 #include <string.h>
 
-int main() {
-    key_t key = ftok("shared_memory", 65);
-    int shmid = shmget(key, 1024, 0666|IPC_CREAT);
-    char *shared_data = (char*) shmat(shmid, (void*)0, 0);
+#define FILE1 "movie-100k_1.txt"
+#define FILE2 "movie-100k_2.txt"
 
-    if (fork() == 0) {
-        // Child Process 1: Read and process the first file
-        printf("Child 1 is processing...\n");
-        // Read File 1, compute average, write to shared_data
-        strcpy(shared_data, "Child 1 Average");
-    } else if (fork() == 0) {
-        // Child Process 2: Read and process the second file
-        printf("Child 2 is processing...\n");
-        // Read File 2, compute average, write to shared_data
-        strcat(shared_data, " | Child 2 Average");
-    } else {
-        // Parent Process: Wait for children, access shared memory
-        wait(NULL);
-        printf("Parent reading shared memory: %s\n", shared_data);
+void calculate_average(const char* filename, char* shared_data){
+    FILE* file = fopen(filename, "r");
+    if(file == NULL){
+        perror("ERROR");
+        exit(EXIT_FAILURE);
     }
 
-    // Detach and destroy shared memory
-    shmdt(shared_data);
-    shmctl(shmid, IPC_RMID, NULL);
-    return 0;
+    int movie_id, use_id, rating, timeStamp;
+    int total_rating = 0, count = 0;
+
+    while (fscanf(file, ""%d\t%d\t%d\t%d", &user_id, &movie_id, &rating, &timestamp) != EOF"){
+        count++;
+        total_rating += rating;
+    }
+    fclose(file);
+
+    double average = (double)total_rating/count;
+    sprintf(shared_data, "File: %s, Average Rating: %.2f", filename, average);
+}
+int main() {
+    key_t key = ftok("share_memory", 15);
+    int shmid = shmget(key, 1024, 0666|IPC_CREATE);
+    char *share_data = (char*) shmat(shmid, (void*)0,0);
+    
 }
